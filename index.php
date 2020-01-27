a534b5637508c750973e1f2e184e9c86732194ed
Ahmad Hadi Setiawan <59782873+ahmadhadist@users.noreply.github.com>
2020-01-28 06:36:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
-include_once("awanvision.html");
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=awanwebap;AccountKey=u7s32wWQrmt7TjiFPOfFykLmNxwD/dKe3nM/o7BYznoLAJlfP8OQxjVXpgPggF8IdF5fbbRPdIeGx97ovgYviA==";
+$containerName = "blockblobs";
+
+//create blob client
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+  
+if (isset($_POST['submit'])) {
+  $fileToUpload = $_FILES["fileToUpload"]["name"];
+  $content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
+  echo fread($content, filesize($fileToUpload));
+    
+  $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+  header("Location: index.php");
+} 
+  
+$listBlobsOptions = new ListBlobsOptions();
+$listBlobsOptions->setPrefix("");
+$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+;
 ?>
