5dffb87f03d9b4129c4d2bcd2fbea4c96fd2d84f
Ahmad Hadi Setiawan <59782873+ahmadhadist@users.noreply.github.com>
2020-01-28 05:52:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,31 @@
 <?php
+require_once 'vendor/autoload.php';
+require_once "./random_string.php";
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=awanwebap;AccountKey=u7s32wWQrmt7TjiFPOfFykLmNxwD/dKe3nM/o7BYznoLAJlfP8OQxjVXpgPggF8IdF5fbbRPdIeGx97ovgYviA==";
+$containerName "blockblobs"
+  
+//create blob client
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+  
+if (isset($_POST['submit'])) {
+  $fileToUpload = $_FILES["fileToUpload"]["name"];
+  $content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
+  echo fread($content, filesize($fileToUpload));
+    
+  $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+  header("Location: index.php");
+} 
+  
+$listBlobsOptions = new ListBlobsOptions();
+$listBlobsOptions->setPrefix("");
+$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+
 include_once("awanvision.html");
 ?>
