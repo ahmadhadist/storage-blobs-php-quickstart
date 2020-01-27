0785fd39721a58e3e5552a2f4fd42c11ed02a0a0
Ahmad Hadi Setiawan <59782873+ahmadhadist@users.noreply.github.com>
2020-01-28 05:59:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,24 +8,37 @@ use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=awanwebap;AccountKey=u7s32wWQrmt7TjiFPOfFykLmNxwD/dKe3nM/o7BYznoLAJlfP8OQxjVXpgPggF8IdF5fbbRPdIeGx97ovgYviA==";
+if (isset($_POST['upload'])) {
 
-  
-//create blob client
-$blobClient = BlobRestProxy::createBlobService($connectionString);
-  
-if (isset($_POST['submit'])) {
-  $fileToUpload = $_FILES["fileToUpload"]["name"];
-  $content = fopen($_FILES["fileToUpload"]["tmp_name"], "r");
-  echo fread($content, filesize($fileToUpload));
+    try {
+        $connectionString = "DefaultEndpointsProtocol=https;AccountName=awanwebap;AccountKey=u7s32wWQrmt7TjiFPOfFykLmNxwD/dKe3nM/o7BYznoLAJlfP8OQxjVXpgPggF8IdF5fbbRPdIeGx97ovgYviA==";
+        $blobClient = BlobRestProxy::createBlobService($connectionString);
     
-  $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-  header("Location: index.php");
-} 
-  
-$listBlobsOptions = new ListBlobsOptions();
-$listBlobsOptions->setPrefix("");
-$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+        $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+    
+        $createContainerOptions->addMetaData("key1", "value1");
+        $createContainerOptions->addMetaData("key2", "value2");
+        $container = "blockblobs";
+        // $blobClient->createContainer($container, $createContainerOptions);
 
+        $fileName = $_FILES['file']['name'];
+        $imageFileType = pathinfo($fileName,PATHINFO_EXTENSION);
+
+        $targetDir = "upload/";
+        $targetFile = $targetDir.basename($_FILES['image']['name']);
+        $file = $_FILES['image']['name'];
+        move_uploaded_file($file,$targetFile);
+        $fileToUpload = "upload/".$files.".".$imageFileType;
+        $content = fopen($fileToUpload,"r") or die("Error");
+        $blobClient->createBlockBlob($container, $file, $content);
+        $listBlobsOptions = new ListBlobsOptions();
+        $listBlobsOptions->setPrefix($fileName);
+    } catch (ServiceException $e){
+        echo $e;
+    }
+    
+    
+
+}
 include_once("awanvision.html");
 ?>
